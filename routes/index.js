769741d49bff839e006d70d7cae046462d7944ed
Vee <vharbison1@gmail.com>
2020-01-14 19:23:50 -0500

    Have the modal working and dynamically renders when user clicks a day in calendar

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,32 @@ const Calendar = require('calendar').Calendar;
 
 // Index Page For Now
 router.get('/', function(req, res) {
-    var cal = new Calendar(1);               // weeks starting on Monday
-    m = cal.monthDays(2020, 1);
-    for (i=0; i<m.length; i++) console.log(m[i]);
+    let cal = new Calendar(1);               // weeks starting on Monday
+    const daysInMonth = cal.monthDays(2020, today('getMonth'));
+    for (i=0; i<daysInMonth.length; i++) console.log(daysInMonth[i]);
 
-    let calenderObject = { calendarValues: m};
+    let juice = 'RANDOM';
+    let calenderObject = { m : daysInMonth, year : today('getYear'), month : today('getMonth')};
 
     res.render('pages/index', calenderObject);
 });
 
-function getTodaysMonth()
+function today(userDateFilter)
 {
+    let d = new Date();
 
+    if(userDateFilter == 'fullDate')
+    {
+        return d.getMonth()+1 + '/' + d.getDate() + '/' + d.getFullYear();
+    }
+    else if (userDateFilter == 'getMonth')
+    {
+        return d.getMonth()+1;
+    }
+    else if(userDateFilter == 'getYear')
+    {
+        return d.getFullYear();
+    }
 }
 
 module.exports = router;
