3fe50161bf6430a2f6712f183f64d0400797f161
Rhonda Gilliard <amourg08@gmail.com>
2020-01-14 20:20:20 -0500

    finished user page - able to create new users

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require ('bcrypt');
+const db = require('../models');
 
 // Index Page For Now
 router.get('/', function(req, res) {
     res.render('pages/register');
 });
+
+router.post('/', function(req, res, next){
+    console.log(req.body)
+    const newUser = db.user.build({
+        email: req.body.email,
+        password: generateHash(req.body.password)
+    });
+    return newUser.save().then(results => {
+        res.redirect('/login');
+    })
+});
+
 module.exports = router;
 
+
+//Generate encrypt password on db
+const generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+//Register new user
